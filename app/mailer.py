8bb651108ac8d843690e6b11559079f14f998318
Nazar868 <zagariynazar04@gmail.com>
2026-07-07 22:26:17 +0300

    Delete app directory

--- app/mailer.py
+++
@@ -1,2 +0,0 @@
-def send_reset_email(email, token):
-    print(f"Reset link: http://localhost:8000/reset?token={token}")
--- app/roles.py
+++
@@ -1,6 +0,0 @@
-from fastapi import HTTPException
-
-
-def require_role(user, role="admin"):
-    if user.get("role") != role:
-        raise HTTPException(status_code=403, detail="Forbidden")
